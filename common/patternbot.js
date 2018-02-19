ec1cd3acca758c678a0393d4ea0891dacecc7186
Thomas J Bradley <hey@thomasjbradley.ca>
2018-02-19 16:17:07 -0500

    More updates!

--- common/patternbot.js
+++ common/patternbot.js
@@ -348,9 +348,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/thomasjbradley/Dropbox/www/patternbot-test-repos/--all-tester
- * @version 1519074266981
+ * @version 1519075004912
  */
-const patternManifest_1519074266980 = {
+const patternManifest_1519075004912 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -780,5 +780,5 @@ const patternManifest_1519074266980 = {
   }
 };
 
-patternBotIncludes(patternManifest_1519074266980);
+patternBotIncludes(patternManifest_1519075004912);
 }());
